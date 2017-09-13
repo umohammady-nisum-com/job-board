71d31df3b092022cc6bcbc1c02bc36d6ae287603
Umair Mohammady <Umair@Umairs-MacBook-Pro.local>
2017-09-13 10:30:16 -0700

    hopefully this works

---
+++ src/app/Models/model.ts
@@ -0,0 +1,16 @@
+export class JobListing {
+
+	firstName: string;
+	lastName: string;
+	email: string;
+	phone: string;
+	title: string;
+	description: string;
+	qualifications: string;
+	salary: number; 
+	jobType: string; 
+	apply: string;
+	location: string;
+
+}
+
---
+++ src/app/newListing/newListing.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core'
+import { Router }	from '@angular/router'
+
+import { JobListing } from '../Models/model' 
+
+import { AppService } from '../../services/app.service'
+
+
+@Component ({
+	templateUrl: 'newListing.component.html',
+	styleUrls: ['./newListing.component.css']
+})
+
+
+export class NewListing implements OnInit {
+
+	jobListing: JobListing 
+
+	constructor(private router: Router,
+				private appService: AppService){}
+
+	ngOnInit(){
+
+	this.jobListing = {
+		firstName: '',
+		lastName: '',
+		email: '',
+		phone: '',
+		title: '',
+		description: '',
+		qualifications: '',
+		salary: null,
+		jobType: '',
+		apply: '',
+		location: ''
+
+		}
+	}
+
+
+	cancel(){
+		this.router.navigate(['allListings'])
+	}
+
+	onSubmit(){
+		this.appService.saveData(this.jobListing)
+	}
+
+}
